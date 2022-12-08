42b19e43c191c89b33f8bb55feabd21ee40c0b90
TRCCompSci <36030019+TRCCompSci@users.noreply.github.com>
2022-12-08 08:47:27 +0000

    Update Program2.cs

--- FSM/Program2.cs
+++ FSM/Program2.cs
@@ -10,15 +10,16 @@ namespace FSM
     {
         enum State
         {
-            S0,
             S1,
             S2,
+            S3,
             Error
         }
         static void Main(string[] args)
         {
             // set the starting state
             State machine = State.S1;
+            // set the accept state
             State accept = State.S2;
             bool control = true;
             while (control)
@@ -26,46 +27,48 @@ namespace FSM
                 Console.WriteLine("You are currenty on state: " + machine.ToString());
                 Console.WriteLine("Please enter an input:");
 
-                int input = Convert.ToInt32(Console.ReadLine());
+                string input = Console.ReadLine();
 
                 switch (input)
                 {
-                    case 0:
-                    case 1:
-                    case 2:
-                    case 3:
-                    case 4:
-                    case 5:
-                    case 6:
-                    case 7:
-                    case 8:
-                    case 9:
-                        if (machine == State.S0)
+                    case "0":
+                    case "1":
+                    case "2":
+                    case "3":
+                    case "4":
+                    case "5":
+                    case "6":
+                    case "7":
+                    case "8":
+                    case "9":
+                        if (machine == State.S1)
                         {
                             machine = State.S2;
                         }
-                        else if (machine == State.S1)
-                        {
-                            machine = State.S0;
-                        }
                         else if (machine == State.S2)
                         {
-                            machine = State.Error;
-                            control = false;
+                            machine = State.S2;
                         }
-
-                        if (machine==accept)
+                        else if (machine == State.S3)
                         {
-                            Console.WriteLine("You are now on the accept state");
+                            machine = State.S3;
                         }
                         break;
+                    case "-":
+                    case "+":
+                        // ADD YOUR LOGIC HERE
+                        break
                     default:
                         Console.WriteLine("Value not valid");
                         control = false;
+                        machine=State.Error;
                         break;
                 
                 }
-
+                if (machine==accept)
+                        {
+                            Console.WriteLine("You are now on the accept state");
+                        }
 
             }
             Console.ReadLine();
