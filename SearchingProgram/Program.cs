0f47a421eeb1e9cf37496ad380b1e55eb9e2d2db
TRCCompSci <36030019+TRCCompSci@users.noreply.github.com>
2023-02-01 13:49:46 +0000

    Create Program.cs

---
+++ SearchingProgram/Program.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchingProgram
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            // declare variables
+            const int MaxListSize = 1000;
+            string MenuItem;
+            int[] NumList = new int[MaxListSize];
+            // program loop
+            do
+            {
+                // display menu and get menu choice
+                MenuItem = GetMenuChoice();
+                // do something with the menu choice
+                switch (MenuItem)
+                {
+                    case "1":
+                        Array.Clear(NumList,0,0);
+                        Random RanNum = new Random();
+                        for (int i = 0; i < MaxListSize; i++) {
+                            NumList[i] = RanNum.Next(1,10000);
+                        } 
+                        break;
+                    case "2":
+                        for (int i = 0; i < MaxListSize; i++)
+                        {
+                            Console.WriteLine(NumList[i]);
+                        }
+                        break;
+                    case "3":
+                        //uncomment out the line below for Binary Search
+                        // Array.Sort(NumList); 
+                        SearchList(ref NumList);
+                        break;
+                    case "4":
+                        break;
+                    default:
+                        Console.WriteLine("invalid selection");
+                        break;
+                }
+            }
+            while (MenuItem != "4");
+            Console.WriteLine("end of program");
+            Console.ReadLine();
+        }
+
+        public static void SearchList(ref int[] TheList)
+        {
+
+        }
+
+        public static string GetMenuChoice()
+        {
+            //display menu
+            Console.WriteLine();
+            Console.WriteLine("1) Generate List");
+            Console.WriteLine("2) Display List");
+            Console.WriteLine("3) Search List");
+            Console.WriteLine("4) Exit program");
+            Console.WriteLine();
+            Console.WriteLine("Enter your menu choice number");
+            // get choice and return the value
+            return Console.ReadLine();
+        }
+    }
+}
+
